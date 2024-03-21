7fff2d2a8903e9d7e0a38f5dda332f55a3b6f463
Joba Adewumi <jobaadewumis@gmail.com>
2024-03-21 18:06:38 +0100

    Initial commit

---
+++ Frontend/owomi/android/app/src/main/kotlin/com/example/owomi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.owomi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Frontend/owomi/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import shared_preferences_foundation
+import webcrypto
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WebcryptoPlugin.register(with: registry.registrar(forPlugin: "WebcryptoPlugin"))
+}
