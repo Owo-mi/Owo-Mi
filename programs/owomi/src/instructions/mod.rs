5434a15ec2e261443bb69aa6207606d58d8508ca
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2024-03-30 14:41:53 +0100

    build code and add idl

--- programs/owomi/src/instructions/mod.rs
+++ programs/owomi/src/instructions/mod.rs
@@ -1,4 +1,11 @@
-// pub mod deposit_token;
+pub use deposit_token::*;
+pub use initialize_account::*;
+pub use initialize_savings::*;
+pub use initialize_token_reserve::*;
+pub use topup_savings::*;
+pub use withdraw_savings::*;
+pub use withdraw_token::*;
+
 pub mod deposit_token;
 pub mod initialize_account;
 pub mod initialize_savings;
--- programs/owomi/src/lib.rs
+++ programs/owomi/src/lib.rs
@@ -1,14 +1,6 @@
 use anchor_lang::prelude::*;
 
-use instructions::{
-    deposit_token::{self, DepositToken},
-    initialize_account::{self, InitializeAccount},
-    initialize_savings::{self, InitializeSavings, InitializeSavingsArgs},
-    initialize_token_reserve::{self, InitializeTokenReserve},
-    topup_savings::{self, TopupSavings, TopupSavingsArgs},
-    withdraw_savings::{self, WithdrawSavings},
-    withdraw_token::{self, WithdrawToken},
-};
+pub use instructions::*;
 
 pub mod instructions;
 pub mod state;
@@ -22,15 +14,19 @@ pub mod owomi {
     pub fn initialize_token_reserve(ctx: Context<InitializeTokenReserve>) -> Result<()> {
         initialize_token_reserve::initialize_token_reserve(ctx)
     }
+
     pub fn initialize_account(ctx: Context<InitializeAccount>) -> Result<()> {
         initialize_account::initialize_account(ctx)
     }
+
     pub fn deposit_token(ctx: Context<DepositToken>, amount: u64) -> Result<()> {
         deposit_token::deposit_token(ctx, amount)
     }
+
     pub fn withdraw_token(ctx: Context<WithdrawToken>, amount: u64) -> Result<()> {
         withdraw_token::withdraw_token(ctx, amount)
     }
+
     pub fn initialize_savings(
         ctx: Context<InitializeSavings>,
         args: InitializeSavingsArgs,
@@ -41,6 +37,7 @@ pub mod owomi {
     pub fn topup_savings(ctx: Context<TopupSavings>, args: TopupSavingsArgs) -> Result<()> {
         topup_savings::topup_savings(ctx, args)
     }
+
     pub fn withdraw_savings(ctx: Context<WithdrawSavings>, index: u64) -> Result<()> {
         withdraw_savings::withdraw_savings(ctx, index)
     }
