2c2943cbe03c3af12ebc651471541ee61fbd5b07
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-01 21:50:34 +0100

    Initializing onboarding page for onboarding logic and UI

--- Frontend/owomi/lib/ui/screens/onboarding/onboarding/onboarding.dart
+++ Frontend/owomi/lib/ui/screens/onboarding/onboarding/onboarding.dart
@@ -1,4 +1,5 @@
-import 'package:onboarding/onboarding.dart';
+// import 'package:onboarding/onboarding.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:owomi/common_libs.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -8,20 +9,35 @@ class OnboardingScreen extends StatefulWidget {
   State<OnboardingScreen> createState() => _OnboardingScreenState();
 }
 
+Widget LogoScreen() {
+  return SvgPicture.asset(
+    'assets/images/logo/logo.svg',
+    semanticsLabel: 'Owomi Logo',
+    width: 200,
+  );
+}
+
 class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Onboarding(
-      pages: [
-        PageModel(
-          widget: Center(
-            child: TextButton(
-              child: const Text('Home'),
-              onPressed: () => context.go('/home'),
-            ),
-          ),
-        )
+    return Column(
+      children: [
+        Center(
+          child: LogoScreen(),
+        ),
       ],
     );
   }
 }
+// Onboarding(
+//       pages: [
+//         PageModel(
+//           widget: Center(
+//             child: TextButton(
+//               child: const Text('Home'),
+//               onPressed: () => context.go('/home'),
+//             ),
+//           ),
+//         )
+//       ],
+//     );
