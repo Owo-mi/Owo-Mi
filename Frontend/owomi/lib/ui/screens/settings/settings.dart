6a4fe22ca05cfdc2819b317b8f7e0f6b2e45e167
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-02 22:29:18 +0100

    Added skelenton for settings page

---
+++ Frontend/owomi/lib/ui/screens/settings/settings.dart
@@ -0,0 +1,17 @@
+import 'package:owomi/common_libs.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}
