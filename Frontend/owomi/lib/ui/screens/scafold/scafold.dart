6193bba761da9e6ed9a41141a4a52f65d35106ef
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-02 22:31:02 +0100

    Added general structure for main page

---
+++ Frontend/owomi/lib/ui/screens/scafold/scafold.dart
@@ -0,0 +1,80 @@
+import 'package:owomi/common_libs.dart';
+import 'package:owomi/ui/screens/home/home.dart';
+import 'package:owomi/ui/screens/settings/settings.dart';
+
+class Scafold extends StatefulWidget {
+  const Scafold({super.key});
+
+  @override
+  State<Scafold> createState() => _ScafoldState();
+}
+
+class _ScafoldState extends State<Scafold> {
+  int bottomSelectedIndex = 0;
+
+  PageController pageController = PageController(
+    initialPage: 0,
+    keepPage: true,
+  );
+
+  pageChanged(index) {
+    setState(() {
+      bottomSelectedIndex = index;
+    });
+  }
+
+  Widget buildPageView() {
+    return PageView(
+      controller: pageController,
+      onPageChanged: (index) {
+        pageChanged(index);
+      },
+      children: const [HomeScreen(), SettingsScreen()],
+    );
+  }
+
+  List<BottomNavigationBarItem> buildButtomNavbarItems() {
+    return [
+      const BottomNavigationBarItem(
+        label: 'Home',
+        icon: Icon(
+          Icons.home,
+        ),
+      ),
+      const BottomNavigationBarItem(
+        label: 'Savings',
+        icon: Icon(
+          Icons.chat,
+        ),
+      ),
+      const BottomNavigationBarItem(
+        label: 'Investment',
+        icon: Icon(
+          Icons.bar_chart_rounded,
+        ),
+      ),
+      const BottomNavigationBarItem(
+        label: 'Account',
+        icon: Icon(
+          Icons.portrait_rounded,
+        ),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          pageController.jumpToPage(index);
+        },
+        unselectedItemColor: Colors.blue,
+        selectedItemColor: Colors.blueAccent,
+        currentIndex: bottomSelectedIndex,
+        items: buildButtomNavbarItems(),
+      ),
+      body: buildPageView(),
+    );
+  }
+}
