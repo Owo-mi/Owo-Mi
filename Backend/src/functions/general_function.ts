f27fad725a0c608334ce5c1dc53120a3db6ddc0e
Dozie2001 <davidchidozie402@gmail.com>
2024-05-12 13:51:41 +0100

    Adjusted decrypt and encrypt functions

--- Backend/src/functions/general_function.ts
+++ Backend/src/functions/general_function.ts
@@ -7,12 +7,12 @@ const key = process.env.ENCRYPTION_KEY || '';
 export const encrypt = async (salt: string) => {
     const saltWordArray = CryptoJS.enc.Utf8.parse(salt);
     const encryptedData = CryptoJS.AES.encrypt(saltWordArray, key);
-    return encryptedData;
+    return encryptedData.toString();
 }
 
-export const decrypt = async (cipher: object) => {
+export const decrypt = async (cipher: string) => {
     try {
-        let bytes = CryptoJS.AES.decrypt(cipher.toString(), key);
+        let bytes = CryptoJS.AES.decrypt(cipher, key);
         if (bytes.sigBytes === 0) {
             throw new Error('Decryption failed');
         }
