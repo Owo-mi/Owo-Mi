4ec960b50f128b814e17a55978812ab5fef08551
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-12 20:42:24 +0100

    Added packages for storage, webview and snackbar

--- Frontend/owomi/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Frontend/owomi/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_inappwebview_macos
 import shared_preferences_foundation
 import webcrypto
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   WebcryptoPlugin.register(with: registry.registrar(forPlugin: "WebcryptoPlugin"))
 }
