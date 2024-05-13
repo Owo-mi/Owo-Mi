b4aa199867c058d57b54ffff1e6860444d2b5d61
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-13 12:40:32 +0100

    Removed container color to start structure of page

--- Frontend/owomi/lib/ui/screens/settings/settings.dart
+++ Frontend/owomi/lib/ui/screens/settings/settings.dart
@@ -10,8 +10,47 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return SafeArea(
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 20.0),
+        // color: Colors.blue,
+        child: Column(
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox.fromSize(
+                        size: const Size(70, 70),
+                        child: ClipOval(
+                          child: Material(
+                            color: Colors.grey,
+                            child: IconButton(
+                              icon: const Icon(
+                                Icons.send,
+                                color: Colors.black,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text('jobaadewumis@gmail.com'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
