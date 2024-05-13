68175186d1a735a96cb34843987b26868924c720
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-13 12:38:21 +0100

    Added text themes to clean up reused code

---
+++ Frontend/owomi/lib/styles/theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const boldSmallText =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+
+  static const heading3Text = TextStyle(fontSize: 18);
+  static const boldHeading3Text =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+
+  // static const textColor1 = Color(0xFF060A17);
+
+  // static const textColor2 = Color(0xFF333333);
+
+  // static const dividerColor = Color(0xFFC4CFDD);
+
+  // static const unClickColor = Color(0xFFE0E0E0);
+
+  // static const unClickTextColor = Color(0xFFA6A6A6);
+
+  // static const clickColor = Color(0xFFCD2929);
+
+  // static const clickTextColor = Colors.white;
+
+  // static const buttonColor = Color(0xFF111724);
+
+  // static const mdBackgroundColor = Color(0xFF24272E);
+
+  // static const mdTipColor = Color(0xFF515865);
+
+  // static const borderColor = Color(0xFFBDBDBD);
+}
