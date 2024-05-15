da02f793072b13479432cb2480cf459d69716aca
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-15 02:50:24 +0100

    Tweaked client id string

--- Frontend/owomi/lib/data/constants.dart
+++ Frontend/owomi/lib/data/constants.dart
@@ -3,5 +3,5 @@ class Constant {
   static String website = 'https://zklogin-dart-demo.pages.dev';
   static String replaceUrl = '$website/#id_token=';
   static String googleClientId =
-      '871727533353-jvtm22r3m2bdn2cujcafrb0opo4434p9.apps.googleusercontent.com';
+      '871727533353-u9cgmdv27piis08k5q39r4hpaorfofjk.apps.googleusercontent.com';
 }
