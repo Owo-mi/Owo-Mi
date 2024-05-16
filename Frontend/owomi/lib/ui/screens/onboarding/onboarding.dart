1c5404818af2c438f0b04ff23a2c572f631da603
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-17 00:19:25 +0100

    Changed structure of onboarding page to accomodate pin and biometrics page

--- Frontend/owomi/lib/ui/screens/onboarding/onboarding.dart
+++ Frontend/owomi/lib/ui/screens/onboarding/onboarding.dart
@@ -1,9 +1,9 @@
 // import 'package:onboarding/onboarding.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:owomi/common_libs.dart';
 import 'package:owomi/logic/zklogin.dart';
 import 'package:owomi/provider/zk_login_provider.dart';
-import 'package:sui/sui.dart';
 
 class OnboardingScreen extends ConsumerStatefulWidget {
   const OnboardingScreen({super.key});
@@ -14,8 +14,9 @@ class OnboardingScreen extends ConsumerStatefulWidget {
 
 class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
   final controller = ScrollController();
-  final suiClient = SuiClient(SuiUrls.devnet);
-  var googleFlow = false;
+
+  String enteredPin = '';
+  bool isPinVisible = false;
 
   @override
   void initState() {
@@ -28,6 +29,14 @@ class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
   //   return ChangeNotifierProvider()
   // }
 
+  Widget LogoScreen() {
+    return SvgPicture.asset(
+      'assets/images/logo/logo.svg',
+      semanticsLabel: 'Owomi Logo',
+      width: 200,
+    );
+  }
+
   Widget redirect(BuildContext context) {
     Future(() {
       Zklogin().showSnackBar(context, 'Redirecting');
@@ -38,12 +47,169 @@ class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
     );
   }
 
+  Widget firstView() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: LogoScreen(),
+          ),
+        ),
+        Expanded(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.green),
+                ),
+                child: const Text(
+                  'Get Started',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () =>
+                    ref.read(onboardingStepsProvider.notifier).state = 2,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget ZkloginView() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: LogoScreen(),
+          ),
+        ),
+        Expanded(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                onPressed: () {
+                  context.go('/googlesignin');
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  shadowColor: Colors.transparent,
+                  padding: const EdgeInsets.all(18),
+                  backgroundColor: const Color(0xFF111724),
+                  foregroundColor: Colors.white,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                ),
+                child: SizedBox(
+                  width: 200,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        'assets/images/logo/google/google.svg',
+                        width: 20,
+                        height: 20,
+                      ),
+                      const SizedBox(width: 15),
+                      const Text(
+                        'Sign in with Google',
+                        style: TextStyle(fontSize: 15),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget numButton(int number) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16),
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            if (enteredPin.length < 4) {
+              enteredPin += number.toString();
+            }
+          });
+        },
+        child: Text(
+          number.toString(),
+          style: const TextStyle(
+              fontSize: 24, color: Colors.black, fontWeight: FontWeight.w600),
+        ),
+      ),
+    );
+  }
+
+  Widget inputPinView() {
+    return ListView(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      physics: const BouncingScrollPhysics(),
+      children: [
+        SvgPicture.asset(
+          'assets/images/logo/logo.svg',
+          semanticsLabel: 'Owomi Logo',
+          width: 50,
+        ),
+        const SizedBox(height: 20),
+        const Text(
+          'Enter Your Pin',
+          style: TextStyle(
+              fontSize: 40, color: Colors.black, fontWeight: FontWeight.w600),
+        ),
+        const SizedBox(height: 50),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(4, (index) {
+            return Container();
+          }),
+        ),
+      ],
+    );
+  }
+
+  Widget biometricsView() {
+    return Container();
+  }
+
+  Widget stepRenderer(BuildContext context) {
+    var step = ref.watch(onboardingStepsProvider);
+    var onboardingComplete = ref.watch(onboardingCompleteProvider);
+    if (onboardingComplete) return redirect(context);
+    if (step == 1) {
+      return firstView();
+    } else if (step == 2) {
+      return ZkloginView();
+    } else if (step == 3) {
+      return inputPinView();
+    } else {
+      return biometricsView();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
     var googleSignIn = ref.watch(googleSignInCompleteProvider);
     var zkloginRunning = ref.watch(zkloginInitializeRunningProvider);
     var zkloginComplete = ref.watch(zkloginCompleteProvider);
+
     if (!zkloginComplete) {
       if (!zkloginRunning) {
         if (googleSignIn) {
@@ -61,22 +227,7 @@ class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
             vertical: width < 600 ? 20 : 40,
             horizontal: width < 600 ? 15 : 30,
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // GoogleSignInPage()
-              zkloginComplete ? redirect(context) : Container(),
-              const Text('hi'),
-              ElevatedButton(
-                onPressed: () {
-                  context.go('/googlesignin');
-                },
-                child: const Text('yoo'),
-              ),
-              // googleFlow ? const GoogleSignInPage() : Container()
-              // FirstStepOnboarding()
-            ],
-          ),
+          child: stepRenderer(context),
         ),
       ),
     );
