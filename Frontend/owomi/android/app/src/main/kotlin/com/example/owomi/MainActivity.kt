912814c21c3eaf0c0009a90707ac8a524a813ecc
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-17 17:56:07 +0100

    Setup to accomodate biometrics in android

--- Frontend/owomi/android/app/src/main/kotlin/com/example/owomi/MainActivity.kt
+++ Frontend/owomi/android/app/src/main/kotlin/com/example/owomi/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.owomi
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
