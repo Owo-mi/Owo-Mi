1a32285804605e269de5fe0543d45f13fc848258
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-18 20:06:47 +0100

    Added theme for text and primary color

--- Frontend/owomi/lib/styles/theme.dart
+++ Frontend/owomi/lib/styles/theme.dart
@@ -4,11 +4,26 @@ class AppTheme {
   static const boldSmallText =
       TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
 
-  static const heading3Text = TextStyle(fontSize: 18);
-  static const boldHeading3Text =
-      TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+  static const smallText = TextStyle(fontSize: 18);
 
-  // static const textColor1 = Color(0xFF060A17);
+  static const heading3Text = TextStyle(fontSize: 21);
+  static const boldHeading3Text =
+      TextStyle(fontSize: 21, fontWeight: FontWeight.bold);
+
+  static const heading2Text = TextStyle(fontSize: 27);
+  static const boldHeading2Text =
+      TextStyle(fontSize: 27, fontWeight: FontWeight.bold);
+
+  static const heading1Text = TextStyle(fontSize: 35);
+  static const boldHeading1Text =
+      TextStyle(fontSize: 35, fontWeight: FontWeight.bold);
+
+  static const primaryColor = Color.fromRGBO(0, 55, 63, 0.973);
+
+  static const secondaryColor = Color.fromARGB(255, 100, 196, 102);
+
+  // static const backgroundColor = Color(0xFF679197);
+  static const backgroundColor = Color.fromARGB(255, 116, 151, 156);
 
   // static const textColor2 = Color(0xFF333333);
 
