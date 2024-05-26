67edbd7a4866237991776d428838424fccbe3fdb
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-26 03:34:29 +0100

    Changed website and redirect url

--- Frontend/owomi/lib/data/constants.dart
+++ Frontend/owomi/lib/data/constants.dart
@@ -1,6 +1,6 @@
 class Constant {
-  static String redirectUrl = 'https://zklogin-dart-demo.pages.dev';
-  static String website = 'https://zklogin-dart-demo.pages.dev';
+  static String redirectUrl = 'https://owomi.dev';
+  static String website = 'https://owomi.dev';
   static String replaceUrl = '$website/#id_token=';
   static String googleClientId =
       '871727533353-u9cgmdv27piis08k5q39r4hpaorfofjk.apps.googleusercontent.com';
