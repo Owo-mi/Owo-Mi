1fa0d0d3f57012328f0dffad5e89b4117aa5c010
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-27 14:20:40 +0100

    Added backend url

--- Frontend/owomi/lib/data/constants.dart
+++ Frontend/owomi/lib/data/constants.dart
@@ -4,4 +4,6 @@ class Constant {
   static String replaceUrl = '$website/#id_token=';
   static String googleClientId =
       '871727533353-u9cgmdv27piis08k5q39r4hpaorfofjk.apps.googleusercontent.com';
+
+  static String backendUrl = 'https://3159-197-211-58-112.ngrok-free.app/api';
 }
