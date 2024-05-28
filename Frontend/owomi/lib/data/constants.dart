e0c1ce10e6cbd2c0e51e093a7c22755da637a1e1
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-28 01:56:18 +0100

    Changed backend url to render and added constants for coin types

--- Frontend/owomi/lib/data/constants.dart
+++ Frontend/owomi/lib/data/constants.dart
@@ -5,5 +5,15 @@ class Constant {
   static String googleClientId =
       '871727533353-u9cgmdv27piis08k5q39r4hpaorfofjk.apps.googleusercontent.com';
 
-  static String backendUrl = 'https://3159-197-211-58-112.ngrok-free.app/api';
+  static String backendUrl = 'https://f91f-197-211-59-16.ngrok-free.app/api';
+}
+
+class CoinType {
+  static String USDC =
+      "0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin::COIN";
+  static String USDT =
+      "0xc060006111016b8a020ad5b33834984a437aaa7d3c74c18e09a95d48aceab08c::coin::COIN";
+  static String BUCK =
+      "0xce7ff77a83ea0cb6fd39bd8748e2ec89a3f41e8efdc3f4eb123e0ca37b184db2::buck::BUCK";
+  static String SUI = "0x2::sui::SUI";
 }
