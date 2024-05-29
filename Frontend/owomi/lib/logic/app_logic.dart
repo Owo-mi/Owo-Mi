255dbe1d8471502ac970919df2c986703320fa0e
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-30 00:50:18 +0100

    Added functions to partially show sensitive strings

--- Frontend/owomi/lib/logic/app_logic.dart
+++ Frontend/owomi/lib/logic/app_logic.dart
@@ -40,4 +40,32 @@ class AppLogic {
       }
     }
   }
+
+  truncateString(String input) {
+    if (input.length <= 8) {
+      return input;
+    }
+    String firstPart = input.substring(0, 4);
+    String lastPart = input.substring(input.length - 4);
+    return '$firstPart...$lastPart';
+  }
+
+  truncateEmail(String email) {
+    List<String> parts = email.split('@');
+    if (parts.length != 2) {
+      return email;
+    }
+
+    String username = parts[0];
+    String domain = '@${parts[1]}';
+
+    if (username.length <= 6) {
+      return email;
+    }
+
+    String firstPart = username.substring(0, 4);
+    String lastPart = username.substring(username.length - 4);
+
+    return '$firstPart...$lastPart$domain';
+  }
 }
