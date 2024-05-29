12fb6168e3b62af15c41f945bacabde786ed6de0
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-29 02:42:48 +0100

    Cleaned up redundant code, added fade and added dynamic route for savings form

--- Frontend/owomi/lib/router.dart
+++ Frontend/owomi/lib/router.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:owomi/ui/screens/onboarding/google_sign_in.dart';
 import 'package:owomi/ui/screens/onboarding/onboarding.dart';
+import 'package:owomi/ui/screens/savings/savings_form.dart';
 import 'package:owomi/ui/screens/scafold/scafold.dart';
 
 /// Shared paths / urls used across the app
@@ -10,30 +11,34 @@ class ScreenPaths {
   // static String splash = '/';
   static String onboarding = '/';
   static String googlesignin = '/googlesignin';
-  // static String first_step = '/';
-  // static String onboarding = '/welcome';
   static String scafold = '/scafold';
-  // static String settings = '/settings';
+  static String savingsForm = '/savingsForm';
 }
 
 final appRouter = GoRouter(
-    // redirect: _handleRedirect,
-    routes: [
-      // AppRoute(ScreenPaths.first_step, (s) => const FirstStepOnboarding()),
-      AppRoute(ScreenPaths.onboarding, (s) => const OnboardingScreen()),
-      AppRoute(ScreenPaths.googlesignin, (s) => const GoogleSignInPage()),
-      // AppRoute(
-      //     ScreenPaths.splash,
-      //     (_) => Container(
-      //           color: Colors.purple,
-      //         )),
-      AppRoute(ScreenPaths.scafold, (s) => const Scafold()),
-      // AppRoute(ScreenPaths.home, (s) => const SettingsScreen())
-    ]);
+  // redirect: _handleRedirect,
+  routes: [
+    AppRoute(ScreenPaths.onboarding, (s) => const OnboardingScreen()),
+    AppRoute(ScreenPaths.googlesignin, (s) => const GoogleSignInPage()),
+    // AppRoute(
+    //     ScreenPaths.splash,
+    //     (_) => Container(
+    //           color: Colors.purple,
+    //         )),
+    AppRoute(ScreenPaths.scafold, (s) => const Scafold()),
+    AppRoute(
+      ScreenPaths.savingsForm,
+      (s) => SavingsFormScreen(
+        savingType: s.uri.queryParameters["savingsType"],
+        savingName: s.uri.queryParameters["savingsName"],
+      ),
+    ),
+  ],
+);
 
 class AppRoute extends GoRoute {
   AppRoute(String path, Widget Function(GoRouterState s) builder,
-      {List<GoRoute> routes = const [], this.useFade = false})
+      {List<GoRoute> routes = const [], this.useFade = true})
       : super(
           path: path,
           routes: routes,
