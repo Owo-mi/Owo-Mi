57ce01d621f4562a6e1ac588209df42e53ba850e
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-29 02:44:07 +0100

    Created savings home page

--- Frontend/owomi/lib/ui/screens/savings/savings.dart
+++ Frontend/owomi/lib/ui/screens/savings/savings.dart
@@ -8,6 +8,114 @@ class SavingsScreen extends StatefulWidget {
 }
 
 class _SavingsScreenState extends State<SavingsScreen> {
+  var savingsCardInfo = [
+    {
+      "Name": "Regular Savings",
+      'Description': 'Reach a specific goal with your circle.',
+      'key': "1",
+      "Color": Colors.greenAccent[600],
+    },
+    {
+      "Name": "Target Savings",
+      'Description': 'Reach a specific goal with your circle.',
+      'key': "2",
+      "Color": Colors.redAccent[600],
+    },
+    {
+      "Name": "Strict Savings",
+      'Description': 'Reach a specific goal with your circle.',
+      'key': "3",
+      "Color": Colors.orangeAccent[600],
+    },
+    {
+      "Name": "Savings Circle",
+      'Description': 'Reach a specific goal with your circle.',
+      'key': "4",
+      "Color": Colors.blueAccent[600],
+    }
+  ];
+
+  Color? selectColor(key) {
+    if (key == "1") {
+      print('wetin dey zuzu');
+      return Colors.greenAccent[600];
+    } else if (key == "2") {
+      return Colors.redAccent[600];
+    } else if (key == "3") {
+      return Colors.orangeAccent[600];
+    } else {
+      return Colors.blueAccent[600];
+    }
+  }
+
+  Widget balance() {
+    return Center(
+      child: Container(
+        width: 350,
+        height: 85,
+        constraints: const BoxConstraints(maxHeight: 150.0),
+        margin: const EdgeInsets.only(left: 10.0, right: 20.0),
+        decoration: BoxDecoration(
+          color: Colors.green,
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: const Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(15, 10, 7, 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 20,
+                    child: Text('Total Savings'),
+                  ),
+                  Text('15,000 sui', style: AppTheme.boldHeading1Text),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget savingsCard(String title, String description, String key) {
+    return InkWell(
+      onTap: () {
+        context.push("/savingsForm?savingsType=$key&savingsName=$title");
+      },
+      child: Center(
+        child: Container(
+          width: 170,
+          height: 130,
+          // constraints: const BoxConstraints(maxHeight: 40.0),
+          // margin: const EdgeInsets.only(left: 5.0, right: 5.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          color: selectColor(key) ?? Colors.blueAccent[600],
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  title,
+                  style: AppTheme.boldHeading3Text,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(description),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +124,38 @@ class _SavingsScreenState extends State<SavingsScreen> {
         title: const Text(
           'Savings',
         ),
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+        children: [
+          balance(),
+          const SizedBox(
+            height: 30,
+          ),
+          const Text(
+            'Save your Tokens',
+            style: AppTheme.boldHeading3Text,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          GridView.count(
+            shrinkWrap: true,
+            crossAxisCount: 2,
+            crossAxisSpacing: 5.0,
+            mainAxisSpacing: 5.0,
+            children: savingsCardInfo
+                .map(
+                  (saving) => savingsCard(
+                    saving['Name'] as String,
+                    saving['Description'] as String,
+                    saving['key'] as String,
+                  ),
+                )
+                .toList(),
+          )
+        ],
       ),
     );
   }
