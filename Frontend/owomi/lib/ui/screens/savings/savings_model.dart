0ab8dfd2c0ffb1359b3de29709b7675461092575
Joba Adewumi <jobaadewumis@gmail.com>
2024-05-29 02:43:44 +0100

    Savings model

---
+++ Frontend/owomi/lib/ui/screens/savings/savings_model.dart
@@ -0,0 +1,15 @@
+// class SavingsForm {
+//   final int key;
+
+//   // final String name;
+//   // final ColorSwatch color;
+//   // final String iconLocation;
+//   // final List<Unit> units;
+
+//   // const Category({
+//   //   required this.name,
+//   //   required this.color,
+//   //   required this.iconLocation,
+//   //   required this.units,
+//   // });
+// }
